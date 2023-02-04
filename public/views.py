22c363e8891197ff48e3d6336cfbd4fe79d33f7f
buschinelli-joao <j.a.c.buschinelli@gmail.com>
2023-02-04 11:08:15 -0300

    created a route to PortSwiggerLabs.html

--- public/views.py
+++ public/views.py
@@ -1,10 +1,13 @@
 ## ROUTES
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 
+@views.route('/labs')
+def labs():
+    return render_template("PortSwiggerLabs.html")
