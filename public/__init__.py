924649fd481ceff4393e9ac1da93c88ff44988cb
buschinelli-joao <j.a.c.buschinelli@gmail.com>
2023-02-04 17:58:23 -0300

    creating database and initializing it

--- public/__init__.py
+++ public/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__) # represents the name of the file
     app.config['SECRET_KEY'] = 'oko3j8jfssj29kflsmk19'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
