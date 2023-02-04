9060664481c13725406b9977b3aead4d35e0a296
buschinelli-joao <j.a.c.buschinelli@gmail.com>
2023-02-03 22:09:22 -0300

    registered views, auth blueprints

--- public/__init__.py
+++ public/__init__.py
@@ -4,6 +4,12 @@ def create_app():
     app = Flask(__name__) # represents the name of the file
     app.config['SECRET_KEY'] = 'oko3j8jfssj29kflsmk19'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
 
 
