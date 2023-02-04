34bba6fc045dc4cd1cfbec1b54b05e450708c3d2
buschinelli-joao <j.a.c.buschinelli@gmail.com>
2023-02-04 19:26:55 -0300

    simple database schema

---
+++ public/models.py
@@ -0,0 +1,19 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))           # one-to-many id fetched from User class (db uses lowercase chars)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')                                     # Stores different notes a specific user owns (Foreign keys 
+                                                                        # first char is capital)
+
