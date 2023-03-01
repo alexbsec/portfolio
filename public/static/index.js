aeb293d19570bce780ed05f0bf5880bd736974e0
buschinelli-joao <j.a.c.buschinelli@gmail.com>
2023-03-01 00:04:23 -0300

    cleaning else ifs and added click effect on nav elements

--- public/static/index.js
+++ public/static/index.js
@@ -9,70 +9,34 @@ function typeWriter(text, speed, i) {
 }
 
 window.onload = function() {
-  var sPath = window.location.pathname;
-  var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
-  var txt;
-  var speed = 70;
-  if (sPage == ""){
-    txt = 'sudo apt-get install me';
-  }
-  else if (sPage == "labs"){
-    txt = 'PortSwigger Labs';
-  }
-  else if (sPage == "login") {
-    txt = 'Login';
-  }
-  else if (sPage == "signup") {
-    txt = 'Sign-Up';
-  }
-  else if (sPage == "projects") {
-    txt = 'Projects';
-  }
-  else if (sPage == "encryption-projects") {
-    txt = 'Encryption Projects';
-  }
-  else if (sPage == "one-time-pad-encryption") {
-    txt = 'One-time Pad Encryption Algorithm in C++'
-  }
-  else if (sPage == "keylogger") {
-    txt = 'Remote Server Keylogger';
-  }
-  else if (sPage == "prevent-sqli") {
-    txt = 'Preventing SQL Injection';
-  }
-  else if (sPage == "encryption-malware") {
-    txt = 'Encryption Malware in C++';
-  }
-  else if (sPage == "sqli01") {
-    txt = 'SQLi Vulnerability in Where Clause Allowing Retrieval of hidden data';
-  }
-  else if (sPage == "sqli02") {
-    txt = 'SQLi Vulnerability Allowing Login Bypass';
-  }
-  else if (sPage == "buslogic01") {
-    txt = 'Excessive trust in Client-Side Controls';
-  }
-  else if (sPage == "buslogic02"){
-    txt = 'High-Level Logic Vulnerability';
-  }
-  else if (sPage == "priv-esc01") {
-    txt = 'Unprotected Admin Functionality';
-  }
-  else if (sPage == "priv-esc02") {
-    txt = 'Unprotected Admin Functionality with Unpredictable URL';
-  }
-  else if (sPage == "priv-esc03") {
-    txt = 'User role Controlled by Request Parameter';
-  }
-  else if (sPage == "priv-esc04") {
-    txt = 'User role can be modified in user profile';
-  }
-  else if (sPage == "priv-esc05") {
-    txt = 'User ID controlled by Request Parameter'
-  }
-  else if (sPage == "priv-esc11") {
-    txt = 'URL Based Access Control can be Circumvented'
-  }
+  const pageTextMap = {
+    "": "sudo apt-get install me",
+    "labs": "PortSwigger Labs",
+    "login": "Login",
+    "signup": "Sign-Up",
+    "projects": "Projects",
+    "encryption-projects": "Encryption Projects",
+    "one-time-pad-encryption": "One-time Pad Encryption Algorithm in C++",
+    "crib-dragging-attacks": "Crib-dragging attack",
+    "keylogger": "Remote Server Keylogger",
+    "prevent-sqli": "Preventing SQL Injection",
+    "encryption-malware": "Encryption Malware in C++",
+    "sqli01": "SQLi Vulnerability in Where Clause Allowing Retrieval of hidden data",
+    "sqli02": "SQLi Vulnerability Allowing Login Bypass",
+    "buslogic01": "Excessive trust in Client-Side Controls",
+    "buslogic02": "High-Level Logic Vulnerability",
+    "priv-esc01": "Unprotected Admin Functionality",
+    "priv-esc02": "Unprotected Admin Functionality with Unpredictable URL",
+    "priv-esc03": "User role Controlled by Request Parameter",
+    "priv-esc04": "User role can be modified in user profile",
+    "priv-esc05": "User ID controlled by Request Parameter",
+    "priv-esc11": "URL Based Access Control can be Circumvented",
+    "pentests": "Penetration Test Labs"
+  };
+  const sPath = window.location.pathname;
+  const sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+  const txt = pageTextMap[sPage] || "Page Not Found";
+  const speed = 70;
   typeWriter(txt, speed, 0);
 };
 
@@ -88,3 +52,35 @@ function dropDown(element) {
 
 
 }
+
+
+document.querySelectorAll(".nav-link").forEach(link => {
+  link.addEventListener("mousemove", e => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    e.target.style.setProperty("--x", `${x}px`);
+    e.target.style.setProperty("--y", `${y}px`);
+  });
+});
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link, .navbar-brand');
+
+function scaleDown(e) {
+  e.target.style.transform = 'scale(0.85)';
+}
+
+function scaleUp(e) {
+  e.target.style.transform = '';
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('mousedown', scaleDown);
+  link.addEventListener('mouseup', scaleUp);
+  link.addEventListener('mouseleave', scaleUp);
+});
+
+document.querySelectorAll(".nav-link, .navbar-brand").forEach(function(item) {
+  item.addEventListener("mouseup", function() {
+    item.style.transform = "scale(1)";
+  });
+});
